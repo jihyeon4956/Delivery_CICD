509f1aab66407ab9573db38541729580fc4c927e
Noh Jihyeon <132557632+jihyeon4956@users.noreply.github.com>
2023-09-25 18:35:29 +0900

    Update start.sh

--- scripts/start.sh
+++ scripts/start.sh
@@ -3,6 +3,15 @@
 PROJECT_ROOT="/home/ubuntu/app"
 JAR_FILE="$PROJECT_ROOT/spring-webapp.jar"
 
+# S3에 저장된 파일 읽어오기
+S3_BUCKET_NAME="mini-delivery"
+S3_FILE_PATH="secu/application.properties"  # S3 내부의 파일 경로
+PROPERTIES_PATH="$PROJECT_ROOT/application.properties"  # EC2 인스턴스 내의 경로
+
+# S3에서 application.properties 파일 복사
+aws s3 cp s3://$S3_BUCKET_NAME/$S3_FILE_PATH $PROPERTIES_PATH
+
+
 APP_LOG="$PROJECT_ROOT/application.log"
 ERROR_LOG="$PROJECT_ROOT/error.log"
 DEPLOY_LOG="$PROJECT_ROOT/deploy.log"
